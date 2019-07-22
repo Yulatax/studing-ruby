e296d375f413db5440d8d9ae233ab3bb31687f65
Pavel Taxidi <paveltaxidi@MacBook-Pro-Pavel.local>
2019-07-23 00:33:17 +0300

    Lesson 6

---
+++ Lesson6/Screencast/car.rb
@@ -0,0 +1,59 @@
+class Car
+  attr_reader :current_rpm
+  attr_accessor :number
+
+  NUMBER_FORMAT = /^[a-z]{1}\d{3}[a-z]{2}$/i
+
+  def initialize(number)
+    @number = number
+    @current_rpm = 0
+    validate!
+  end
+
+  def start_engine
+    start_engine! if engine_stopped?
+  end
+
+  def engine_stopped?
+    current_rpm.zero?
+  end
+
+  def valid?
+    validate!
+    true
+  rescue
+    false
+  end
+
+  protected
+
+  attr_writer :current_rpm
+
+  def validate!
+    raise "Number can't be nil" if number.nil?
+    raise "Number should be at least 6 symbols" if number.length < 6
+    raise "Number has invalid format" if number !~ NUMBER_FORMAT
+  end
+
+  def initial_rpm
+    700
+  end
+
+  def start_engine!
+    self.current_rpm = initial_rpm
+  end
+end
+
+# car = Car.new('123456')
+# p car
+# regexp = /.+/
+# p "" =~ regexp
+# p "a" =~ regexp
+# regexp = /\d+.+\d*/
+# p "a" =~ regexp
+# p "1a" =~ regexp
+# p "1a2" =~ regexp
+# p "1ab" =~ regexp
+
+car = Car.new("a245fs" )
+p car
--- Lesson6/Screencast/main.rb
+++ Lesson6/Screencast/main.rb
@@ -1,10 +1,52 @@
-begin
-puts 'Before exception'
-  Math.sqrt(-1)
+# begin
+# puts 'Before exception'
+#   Math.sqrt(-1)
+#
+# rescue StandardError => e
+#   puts "Error!!!"
+#   raise
+# rescue NoMemoryError => e
+#   puts "No memory!!!"
+# end
+#
+# puts 'After exception'
+# ___________________________
 
-rescue StandardError => e
-  puts "Exception: #{e.message}"
-  puts e.backtrace.inspect
+# def method_with_error
+#   # ...
+#   raise ArgumentError, "Oh no!"
+# end
+#
+# begin
+#   method_with_error
+# rescue RuntimeError => e
+#   puts e.inspect
+# end
+#
+# puts "After exception"
+# ____________________________
+
+# def sqrt(value)
+#   sqrt = Math.sqrt(value)
+#   puts sqrt
+# rescue StandardError
+#   puts "Incorrect value"
+# end
+#
+# sqrt(-1)
+# _____________________________
+
+def connect_to_wikipedia
+  # ...
+  raise "Connection error"
 end
 
-puts 'After exception'
+attempt = 0
+begin
+  connect_to_wikipedia
+rescue RuntimeError
+  attempt += 1
+  retry if attempt < 3
+ensure
+  puts "There was #{attempt} attempts"
+end
